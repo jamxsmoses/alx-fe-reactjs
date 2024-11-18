c5a2916307bc3ba270324db269d4b675a0a03d94
James Moses <jamxsmosxs@gmail.com>
2024-11-18 12:10:25 +0100

    Added User Profile Card

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import WelcomeMessage from "./components/WelcomeMessage";
 import Header from "./components/Header";
 import UserProfile from "./components/UserProfile";
@@ -7,8 +6,6 @@ import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <WelcomeMessage />
