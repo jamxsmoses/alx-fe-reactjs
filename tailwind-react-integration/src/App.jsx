c5916d77f1df7dde49abc39f68c3d9ff95c50ed8
James Moses <jamxsmosxs@gmail.com>
2025-01-07 14:28:58 +0100

    Added interactivity to elements on userProfile.jsx

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,18 +1,14 @@
-import WelcomeMessage from "./components/WelcomeMessage";
-import Header from "./components/Header";
+// import WelcomeMessage from "./components/WelcomeMessage";
+// import Header from "./components/Header";
 import UserProfile from "./components/UserProfile";
-import MainContent from "./components/MainContent";
-import Footer from "./components/Footer";
+// import MainContent from "./components/MainContent";
+// import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <WelcomeMessage />
-      <Header />
-      <MainContent />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-      <Footer />
     </>
   );
 }
