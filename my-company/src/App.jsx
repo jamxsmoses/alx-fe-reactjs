eff784c51120f016f6448b2d8fd3a7662a7e3dfd
James Moses <jamxsmosxs@gmail.com>
2025-01-07 04:09:37 +0100

    Added my-company

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./Home";
 import About from "./About";
 import Services from "./Services";
 import Contact from "./Contact";
-import Navbar from "./Navbar";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
