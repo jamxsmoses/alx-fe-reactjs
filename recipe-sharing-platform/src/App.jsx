1766dee0634cf02a9243fc16d8b5088cb5ba7343
James Moses <jamxsmosxs@gmail.com>
2025-01-17 12:14:44 +0100

    Added RecipeDetails.jsx

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,17 +1,19 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./components/Homepage";
 import RecipeDetail from "./components/RecipeDetail";
 
 function App() {
   return (
     <>
-      <div className="app box-border p-5 bg-black content-center justify-items-center">
-        <Routes>
-          <Route path="" element={<Homepage />} />
-          <Route path="/:id" element={<RecipeDetail />} />
-        </Routes>
-      </div>
+      <BrowserRouter>
+        <div className="app box-border p-5 bg-black content-center justify-items-center">
+          <Routes>
+            <Route path="" element={<Homepage />} />
+            <Route path="/:id" element={<RecipeDetail />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </>
   );
 }
